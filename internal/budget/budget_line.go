8e8e680c6f09015e85c631f3b9ba168cd967439c
Chase Rogness <chaserogness@gmail.com>
2026-03-23 16:05:22 -0500

    Budget Line Allocation completed and tested

--- internal/budget/budget_line.go
+++ internal/budget/budget_line.go
@@ -1,10 +1,14 @@
 package budget
 
+import "errors"
+
 type BudgetLine struct {
 	Month      string
 	CategoryID CategoryID
 	Budgeted   Money
 }
+
+var ErrInvalidAllocateAmount = errors.New("cannot allocate 0 or negative amounts")
 
 func (line BudgetLine) Available(activity Money) (Money, error) {
 	available, err := line.Budgeted.Add(activity)
@@ -12,4 +16,19 @@ func (line BudgetLine) Available(activity Money) (Money, error) {
 		return 0, err
 	}
 	return available, nil
+}
+
+func (line BudgetLine) Allocate(amount Money) (BudgetLine, error) {
+	if amount.Cents() <= 0 {
+		return BudgetLine{}, ErrInvalidAllocateAmount
+	}
+	newBudgeted, err := line.Budgeted.Add(amount)
+	if err != nil {
+		return BudgetLine{}, err
+	}
+	return BudgetLine{
+		Month:      line.Month,
+		CategoryID: line.CategoryID,
+		Budgeted:   newBudgeted,
+	}, nil
 }
--- internal/budget/budget_line_test.go
+++ internal/budget/budget_line_test.go
@@ -51,3 +51,55 @@ func TestAvailable_BubbleUpError(t *testing.T) {
 		t.Fatal("Available should throw ErrMoneyOverflow when budgeted is maxInt64 plus activity with 1")
 	}
 }
+
+func TestAllocate_BudgetLineAllocation(t *testing.T) {
+	categoryID, _ := NewCategoryID("groceries")
+	var budgetLine BudgetLine = BudgetLine{
+		Month:      time.Now().Format("2006-01"),
+		CategoryID: categoryID,
+		Budgeted:   Money(100),
+	}
+	newBudgetLine, err := budgetLine.Allocate(Money(100))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if newBudgetLine.Budgeted.Cents() != 200 {
+		t.Fatalf("new budgeted after allocation is %d, should be 200", newBudgetLine.Budgeted.Cents())
+	}
+	if newBudgetLine.Month != budgetLine.Month {
+		t.Fatalf("new budgeted month is %s, should be %s", newBudgetLine.Month, budgetLine.Month)
+	}
+	if newBudgetLine.CategoryID != budgetLine.CategoryID {
+		t.Fatalf("new budgeted category id is %s, should be %s", newBudgetLine.CategoryID.String(), budgetLine.CategoryID.String())
+	}
+}
+
+func TestAllocate_NegativeAllocationError(t *testing.T) {
+	categoryID, _ := NewCategoryID("groceries")
+	var budgetLine BudgetLine = BudgetLine{
+		Month:      time.Now().Format("2006-01"),
+		CategoryID: categoryID,
+		Budgeted:   Money(100),
+	}
+	_, err := budgetLine.Allocate(Money(-1))
+	if !errors.Is(err, ErrInvalidAllocateAmount) {
+		t.Fatal("Allocate should throw ErrInvalidAllocateAmount when 0 or negative is provided")
+	}
+	_, err = budgetLine.Allocate(Money(0))
+	if !errors.Is(err, ErrInvalidAllocateAmount) {
+		t.Fatal("Allocate should throw ErrInvalidAllocateAmount when 0 or negative is provided")
+	}
+}
+
+func TestAllocate_BubbleUpError(t *testing.T) {
+	categoryID, _ := NewCategoryID("groceries")
+	var budgetLine BudgetLine = BudgetLine{
+		Month:      time.Now().Format("2006-01"),
+		CategoryID: categoryID,
+		Budgeted:   Money(math.MaxInt64),
+	}
+	_, err := budgetLine.Allocate(Money(1))
+	if !errors.Is(err, ErrMoneyOverflow) {
+		t.Fatal("Allocate should throw ErrMoneyOverflow when budgeted is maxInt64 and allocation occurs")
+	}
+}
